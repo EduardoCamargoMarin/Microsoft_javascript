59a8bd02174b7518cf9ef7edfd95f4f64510c25a
Eduardo <du.marin10@gmail.com>
2022-12-26 15:46:49 -0300

    Matriz

---
+++ JavaScript/sorvete.js
@@ -0,0 +1,25 @@
+let saborSorvete = ["Chocolate", "Flocos", "Morango", "Creme"] //Sabores de sorvete
+
+console.warn('Sabores clássicos')
+console.log(saborSorvete[0]);
+console.log(saborSorvete[1]);
+console.log(saborSorvete[2]);
+console.log(saborSorvete[3]);
+
+//adicionar mais sabores
+
+saborSorvete.push("Menta");
+saborSorvete.push("Paçoca");
+saborSorvete.push("Açai");
+saborSorvete.push("Ferrero");
+saborSorvete.push("Sonho de Valsa");
+
+//Apresentar os novos sabores
+console.error('Trazendo os novos sabores a partir de janeiro 2023!')
+
+console.log(saborSorvete[4]);
+console.log(saborSorvete[5]);
+console.log(saborSorvete[6]);
+console.log(saborSorvete[7]);
+console.log(saborSorvete[8]);
+
